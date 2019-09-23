72f80b246fe4912f3cdc0ed94830092d5574d65e
Brian Vaughn <bvaughn@fb.com>
2019-09-23 09:25:20 -0700

    Revert change to backend injection method from PR #16752

---
+++ packages/react-devtools-extensions/src/inject.js
@@ -0,0 +1,19 @@
+/* global chrome */
+
+export default function inject(scriptName: string, done: ?Function) {
+  const source = `
+  (function () {
+    window.postMessage({ source: 'react-devtools-inject-script', scriptName: "${scriptName}" }, "*");
+  })()
+  `;
+
+  chrome.devtools.inspectedWindow.eval(source, function(response, error) {
+    if (error) {
+      console.log(error);
+    }
+
+    if (typeof done === 'function') {
+      done();
+    }
+  });
+}
